3b7f331572967c0de2fe6638b04759a7ecfa9979
TheDevelopment <github@eggandjam.com>
2010-06-06 09:59:49 +1000

    active scaffold heroku bug?

--- config/environment.rb
+++ config/environment.rb
@@ -25,11 +25,11 @@ Rails::Initializer.run do |config|
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  config.plugins = [ :active_scaffold, :exception_notification, :ssl_requirement, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
-  config.frameworks -= [ :active_record ]
+  #config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
