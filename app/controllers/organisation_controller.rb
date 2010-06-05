6afbb1228cddf02e40ec2b5bc1b57d26a6d807fd
TheDevelopment <github@eggandjam.com>
2010-06-05 21:17:55 +1000

    signup page

---
+++ app/controllers/organisation_controller.rb
@@ -0,0 +1,22 @@
+class OrganisationController < ApplicationController
+  # render new.rhtml
+  def new
+    @user = User.new
+    @organisation = Organisation.new
+  end
+ 
+  def create
+    logout_keeping_session!
+    @user = User.new(params[:user])
+    @user.register! if @user && @user.valid?
+    success = @user && @user.valid?
+    if success && @user.errors.empty?
+      redirect_back_or_default('/')
+      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+    else
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      render :action => 'new'
+    end
+  end
+  
+end
---
+++ spec/controllers/organisation_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe OrganisationController do
+
+  #Delete this example and add some real ones
+  it "should use OrganisationController" do
+    controller.should be_an_instance_of(OrganisationController)
+  end
+
+end
